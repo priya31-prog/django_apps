309441671ced7dc7be4c1d20425a7b86107b8c7d
priya31-prog <karthika200103@gmail.com>
2025-03-11 22:01:44 +0530

    react app creation

--- ecommerce_web/ecommerce_app/models.py
+++ ecommerce_web/ecommerce_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AccountInfo(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    profile_image = models.CharField(max_length=400)
+    email = models.CharField(max_length=150)
+    password = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "accountinfo"
